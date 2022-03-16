42933ad3f644aa9359ed93b73841f46d660d27d7
laurenbastian <laurenlbastian@gmail.com>
2022-03-16 13:26:29 -0500

    right censor data

--- build_survival_models.R
+++ build_survival_models.R
@@ -6,11 +6,16 @@ library(survminer)
 source("build_survival_dataset.R")
 
 ##KM CURVES
+##Right Censor Data to Age 50
+surv.df$event = as.numeric(surv.df$event)
+surv.df.rc = surv.df[surv.df$start <=50 & surv.df$end > 50,]
+surv.df.rc$start = 50
+
 windowsFonts(A = windowsFont("Times New Roman"))
 
-surv.obj = Surv(time = surv.df.censored$start, 
-                time2 = surv.df.censored$end, 
-                event = surv.df.censored$event == "1")
+surv.obj = Surv(time = surv.df.rc$start, 
+                time2 = surv.df.rc$end, 
+                event = surv.df.rc$event == 1)
 summary(surv.obj)
 
 
@@ -19,47 +24,51 @@ plot(surv.fit.full,
      xlab = "Age", 
      ylab = "Survival Probability", 
      main = "Diabetes Incidence Survival versus Age",
-     xlim = c(40,60))
+     xlim = c(50,60))
 ggsurvplot(surv.fit.full, 
-           data = surv.df.censored, 
+           data = surv.df.rc, 
            conf.int=FALSE, 
            xlab = "Age", 
            ylab = "Survival Probability", 
            main = "Diabetes Incidence Survival versus Age", 
-           xlim = c(40,60),
-           legend = "none")
+           xlim = c(50,60),
+           legend = "none",
+           risk.table = TRUE,
+           risk.table.height = .5,
+           break.x.by = 2,
+           fontsize = 3)
 
-surv.fit.race = survfit(surv.obj ~ surv.df.censored$race)
+surv.fit.race = survfit(surv.obj ~ surv.df.rc$race)
 summary(surv.fit.race)
 plot(surv.fit.race, 
      xlab = "Age", 
      ylab = "Survival Probability", 
      main = "Diabetes Incidence Survival by Race", 
-     xlim = c(40,60))
+     xlim = c(50,60))
 plot.race = ggsurvplot(surv.fit.race, 
-           data = surv.df.censored, 
+           data = surv.df.rc, 
            xlab = "Age", 
            ylab = "Survival Probability", 
            title = "Diabetes Incidence Survival by Race", 
-           xlim = c(40,60), 
+           xlim = c(50,60),
+           ylim = c(0,1),
            palette = c("#E69F00", "#56B4E9", "#009E73", "#D55E00", "#CC79A7"),
            linetype = 1, 
            legend = 'right',
-           legend.labs = c("Black/African American", "Chinese/Chinese American", 
-                           "Japanese/Japanese American", "Caucasian/White Non-Hispanic", 
+           legend.labs = c("Black/African-American", "Chinese/Chinese-American", 
+                           "Japanese/Japanese-American", "Caucasian/White-Non-Hispanic", 
                            "Hispanic"),
            legend.title = "Race/Ethnicity",
-           #risk.table = TRUE,
-           #risk.table.poss = 'out',
-           #tables.height = 0.4,
-           #tables.theme = clean_table_theme(),
            ggtheme = theme_classic2(base_size=12, base_family = "A"),
            font.family = "A",
            font.legend = 9,
-           break.time.by = 5)
+           risk.table = TRUE,
+           risk.table.height = .5,
+           break.x.by = 2, 
+           fontsize = 3)
 print(plot.race)
 
-ggsurvtable(surv.fit.race, 
-            data = surv.df.censored,
-            break.time.by = 5)
+
+
+
 
